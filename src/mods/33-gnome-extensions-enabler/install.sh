1ffcc47e923d377d6aeb9251686b9af0aa022265
MR.X <vaxpsy@gmail.com>
2025-10-05 18:49:10 +0300

    vaxp-up-archmnu

--- src/mods/33-gnome-extensions-enabler/install.sh
+++ src/mods/33-gnome-extensions-enabler/install.sh
@@ -7,7 +7,10 @@ print_ok "Enabling gnome extensions for root..."
 /root/.local/bin/gext -F enable blur-my-shell@aunetx
 /root/.local/bin/gext -F enable ProxySwitcher@flannaghan.com
 /root/.local/bin/gext -F enable customize-ibus@hollowman.ml
-/root/.local/bin/gext -F enable dash-to-panel@jderose9.github.com
+/root/.local/bin/gext -F enable openbar@neuromorph
+/root/.local/bin/gext -F enable tasks-in-panel@fthx
+/root/.local/bin/gext -F enable quick-settings-tweaks@qwreey
+/root/.local/bin/gext -F enable compiz-alike-magic-lamp-effect@hermes83.github.com
 /root/.local/bin/gext -F enable network-stats@gnome.noroadsleft.xyz
 /root/.local/bin/gext -F enable openweather-extension@penguin-teal.github.io
 /root/.local/bin/gext -F enable switcher@anduinos
