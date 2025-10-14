43d946bc3ca36087f57449dc574582a03def5e06
vaxp <vaxpsy@gmail.com>
2025-10-14 06:54:05 +0300

    wl

--- src/mods/23-wallpaper-mod/install.sh
+++ src/mods/23-wallpaper-mod/install.sh
@@ -5,15 +5,15 @@ set -u                  # treat unset variable as error
 print_ok "Cleaning and reinstalling wallpaper"
 rm /usr/share/gnome-background-properties/* -rf
 rm /usr/share/backgrounds/* -rf
-mv ./Fluent-building-night.jpg /usr/share/backgrounds/
-mv ./Fluent-building-light.jpg /usr/share/backgrounds/
+mv ./Fluent-building-night.png /usr/share/backgrounds/
+mv ./Fluent-building-light.png /usr/share/backgrounds/
 cat << EOF > /usr/share/gnome-background-properties/fluent.dark.xml
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
 <wallpapers>
 <wallpaper deleted="false">
 <name>Fluent Building Dark</name>
-<filename>/usr/share/backgrounds/Fluent-building-night.jpg</filename>
+<filename>/usr/share/backgrounds/Fluent-building-night.png</filename>
 <options>zoom</options>
 <shade_type>solid</shade_type>
 </wallpaper>
@@ -25,7 +25,7 @@ cat << EOF > /usr/share/gnome-background-properties/fluent.light.xml
 <wallpapers>
 <wallpaper deleted="false">
     <name>Fluent Building Light</name>
-    <filename>/usr/share/backgrounds/Fluent-building-light.jpg</filename>
+    <filename>/usr/share/backgrounds/Fluent-building-light.png</filename>
     <options>zoom</options>
     <shade_type>solid</shade_type>
 </wallpaper>
